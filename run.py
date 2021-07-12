85cf852adbfb5395c94f19319cb33ca6201dd49f
Banhidy Krisztian <krisztian@peak.com>
2021-07-12 22:23:56 +0300

    Remove slack, update requirements

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
+import os
+
 import schedule
 import time
 import sqlite3
 
 from reconbot.tasks import esi_notification_task
 from reconbot.notifiers.caching import CachingNotifier
-from reconbot.notifiers.slack import SlackNotifier
 from reconbot.notifiers.discord import DiscordNotifier
 from reconbot.notifiers.discordwebhook import DiscordWebhookNotifier
 from reconbot.notifiers.splitter import SplitterNotifier
@@ -18,22 +19,10 @@ from reconbot.sso import SSO
 # ESI notification endpoint cache timer in minutes
 notification_caching_timer = 10
 
-# Slack bot integration API key
-slack_apis = {
-  'example': {
-    'api_key': 'xxxx-xxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx',
-    'username': 'reconbot',
-  },
-}
-
 # Discord bot integration API key and channel
 discord = {
-    'personal': {
-        'token': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-        'channel_id': 'xxxxxxxxxxxxxxxxxx'
-    },
-    'my-webhook': {
-        'url': 'https://discordapp.com/api/webhooks/xxxxxxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    'test-server': {
+        'url': os.environ.get('DISCORD_URL')
     }
 }
 
@@ -41,101 +30,32 @@ discord = {
 # tokens for ESI API. Get them on:
 # https://developers.eveonline.com/applications
 sso_app = {
-    'client_id': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    'secret_key': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    'client_id': os.environ.get('CLIENT_ID'),
+    'secret_key': os.environ.get('CLIENT_SECRET')
 }
 
 # A dictionary of API key groups.
 # Get refresh tokens for your characters by following Fuzzwork's guide:
 # https://www.fuzzwork.co.uk/2017/03/14/using-esi-google-sheets/
 eve_apis = {
-    'fc-team': {
+    'all': {
         'notifications': {
             'whitelist': None, # allow all notification types
         },
         'characters': {
-            'ccp-example-1': {
-                'character_name': 'CCP Example',
-                'character_id': 11111111,
-                'refresh_token': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-            },
-            'ccp-example-2': {
-                'character_name': 'CCP Example 2',
-                'character_id': 33333333,
-                'refresh_token': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-            },
-        },
-    },
-    'logistics-team': {
-        'notifications': {
-            'whitelist': [ # Allow only specified notification types
-                'SovStructureDestroyed',
-                'SovStructureReinforced',
-                'StructureUnderAttack',
-                'OwnershipTransferred',
-                'StructureOnline',
-                'StructureFuelAlert',
-                'StructureAnchoring',
-                'StructureServicesOffline',
-                'StructureLostShields',
-                'StructureLostArmor',
-                'TowerAlertMsg',
-                'StationServiceEnabled',
-                'StationServiceDisabled',
-                'OrbitalReinforced',
-                'OrbitalAttacked',
-                'SovAllClaimAquiredMsg',
-                'SovStationEnteredFreeport',
-                'SovAllClaimLostMsg',
-                'SovStructureSelfDestructRequested',
-                'SovStructureSelfDestructFinished',
-                'StationConquerMsg',
-                'notificationTypeMoonminingExtractionStarted',
-                'MoonminingExtractionFinished',
-                'MoonminingLaserFired',
-            ],
-        },
-        'characters': {
-            'ccp-example-3': {
-                'character_name': 'CCP Example 3',
-                'character_id': 55555555,
-                'refresh_token': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-            },
-            'ccp-example-4': {
-                'character_name': 'CCP Example 4',
-                'character_id': 77777777,
-                'refresh_token': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-            },
+            's4mur4ri-test': {
+                'character_name': os.environ.get('CHARACTER_NAME'),
+                'character_id': os.environ.get('CHARACTER_ID'),
+                'refresh_token': os.environ.get('CHARACTER_REFRESH_TOKEN')
+            }
         },
     }
 }
 
-my_slack_channels = CachingNotifier(
-    SplitterNotifier([
-        SlackNotifier(
-            slack_apis['example']['api_key'],
-            slack_apis['example']['username'],
-            '#fc',
-            'online'
-        ),
-        SlackNotifier(
-            slack_apis['example']['api_key'],
-            slack_apis['example']['username'],
-            '#logistics',
-            'all'
-        )
-    ]),
-    duration=3600
-)
-
 my_discord_channels = CachingNotifier(
     SplitterNotifier([
-        DiscordNotifier(
-            discord['personal']['token'],
-            discord['personal']['channel_id']
-        ),
         DiscordWebhookNotifier(
-            discord['my-webhook']['url']
+            discord['test-server']['url']
         )
     ]),
     duration=3600
@@ -149,24 +69,16 @@ def api_to_sso(api):
         api['character_id']
     )
 
-api_queue_fc = ApiQueue(list(map(api_to_sso, eve_apis['fc-team']['characters'].values())))
-api_queue_logistics = ApiQueue(list(map(api_to_sso, eve_apis['logistics-team']['characters'].values())))
+api_queue_all = ApiQueue(list(map(api_to_sso, eve_apis['all']['characters'].values())))
 
-def notifications_job_fc():
+def notifications_job_all():
     esi_notification_task(
-        eve_apis['fc-team']['notifications'],
-        api_queue_fc,
-        'slack',
-        my_slack_channels
-    )
-
-def notifications_job_logistics():
-    esi_notification_task(
-        eve_apis['logistics-team']['notifications'],
-        api_queue_logistics,
+        eve_apis['all']['notifications'],
+        api_queue_all,
         'discord',
         my_discord_channels
     )
+
 
 
 def run_and_schedule(characters, notifications_job):
@@ -177,8 +89,7 @@ def run_and_schedule(characters, notifications_job):
     notifications_job()
     schedule.every(notification_caching_timer/len(characters)).minutes.do(notifications_job)
 
-run_and_schedule(eve_apis['fc-team']['characters'], notifications_job_fc)
-run_and_schedule(eve_apis['logistics-team']['characters'], notifications_job_logistics)
+run_and_schedule(eve_apis['all']['characters'], notifications_job_all)
 
 while True:
     schedule.run_pending()
